bc35e6dfc73ed354725030c8b6975f2750f2f66e
Anibal Anto <anibal.anto@gmail.com>
2014-12-16 16:08:58 -0300

    agregados llamadas a servicios para cargar datos

--- utils/db-population.sh
+++ utils/db-population.sh
@@ -1,6 +1,13 @@
 #/bin/bash
 
-curl -i -X POST -H "Content-Type:application/json" -d '{  "uVarietyID" : "1",  "name" : "Tomate Perita" }' http://localhost:8080/variety
-curl -i -X POST -H "Content-Type:application/json" -d '{  "uVarietyID" : "2",  "name" : "Tomate Redondo" }' http://localhost:8080/variety
-curl -i -X POST -H "Content-Type:application/json" -d '{  "uVarietyID" : "3",  "name" : "Tomate Amarillo" }' http://localhost:8080/variety
-
+curl -i -X POST -H "Content-Type:application/json" -d '{ "uVarietyID" : "ADFA3885",  "name" : "Tomate Perita" }' http://localhost:8080/variety
+curl -i -X POST -H "Content-Type:application/json" -d '{ "uVarietyID" : "BCFA3775",  "name" : "Tomate Redondo" }' http://localhost:8080/variety
+curl -i -X POST -H "Content-Type:application/json" -d '{ "uVarietyID" : "CBFA3665",  "name" : "Tomate Amarillo" }' http://localhost:8080/variety
+curl -i -X POST -H "Content-Type:application/json" -d '{ "uVarietyID" : "DAFA3555",  "name" : "berenjena" }' http://localhost:8080/variety
+curl -i -X POST -H "Content-Type:application/json" -d '{ "uHarvestID" : "AABBCCDD",  "variety" : "http://localhost:8080/variety/DAFA3555" }' http://localhost:8080/harvest
+curl -i -X POST -H "Content-Type:application/json" -d '{ "uHarvestID" : "AABBCCEE", "mother" : "http://localhost:8080/harvest/AABBCCDD" , "variety" : "http://localhost:8080/harvest/AABBCCDD/variety" }' http://localhost:8080/harvest
+curl -i -X PATCH -H "Content-Type:application/json" -d '{ "shared" : "true" }' http://localhost:8080/harvest/AABBCCEE
+curl -i -X POST -H "Content-Type:application/json" -d '{ "uFarmerID" : "FFAAEE44" }' http://localhost:8080/farmer
+curl -i -X POST -H "Content-Type:application/json" -d '{ "farmerReceptor" : "http://localhost:8080/farmer/FFAAEE44" , "harvest" : "http://localhost:8080/harvest/AABBCCEE" }' http://localhost:8080/interchange
+curl -i -X PATCH -H "Content-Type:application/json" -d ' { "score" : "5" }' http://localhost:8080/interchange/1
+curl -X DELETE http://localhost:8080/variety/BCFA3775
