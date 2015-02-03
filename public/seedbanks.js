bb43dde651fc1523aafdc59b8f516a4bc11be949
pira <manuelalonsod@gmail.com>
2015-02-03 16:39:15 -0300

    Avances con AngularJS

--- public/seedbanks.js
+++ public/seedbanks.js
@@ -2,7 +2,7 @@
 
 	var sb = angular.module('sb', ['ngResource']);
 	
-	var host = '192.168.1.34';
+	var host = '192.168.1.67';
 	var fullhost = 'http://'+host+':8080';
 	
 	sb.controller('VarietyController', ['$http', function($http) {
@@ -13,6 +13,31 @@
 		});
 		
 	}]);
+	
+	// Buscar Harvest compartida por Variedad
+	sb.controller('FindHarvestCtrl', ['$http', function($http) {
+		bank = this;
+		bank.cosechas = [];
+		this.idvariedad = 2;
+		$http.get(fullhost+'/harvest/search/findBySharedAndVariety\?shared=1\&variety='+this.idvariedad).success(function(data){
+			bank.cosechas = data["_embedded"]["harvest"];
+		});
+
+	}]);
+	
+	// Buscar Harvest compartida por Variedad
+	sb.controller('FindHarvestCtrl2', ['$http', function($http) {
+		bank = this;
+		bank.cosechas = [];
+		this.idvariedad = 1;
+		this.query = 1;
+		this.queryFarmer = "/harvest/" + this.query + "/farmer";
+		$http.get(fullhost+'/harvest').success(function(data){
+			bank.cosechas = data["_embedded"]["harvest"];
+		});
+
+	}]);
+	
 	
 	/*/ Ver
 	sb.controller('HarvestController', ['$http', function($http) {
